d9e4d59a9252697f3ea43a9bacd27c84f52278e7
Sipang K <plaaom39@gmail.com>
2023-07-12 14:56:54 +0700

    chore: add Header component to App.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Header.css";
+
+export default function Header() {
+  return (
+    <header>
+      <h3>Shopping Application</h3>
+      <p>Cart : 0</p>
+    </header>
+  );
+}
